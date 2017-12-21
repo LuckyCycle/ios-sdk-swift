466cb1baa2fb69ca90eee387c10a12bf37b3199b
iesta <jm@goliwok.com>
2017-12-21 10:20:04 +0100

    init

---
+++ sdk_luckycycle_ios/sdk_luckycycle_ios.h
@@ -0,0 +1,19 @@
+//
+//  sdk_luckycycle_ios.h
+//  sdk_luckycycle_ios
+//
+//  Created by jm goemaere on 18/12/17.
+//  Copyright © 2017 jm goemaere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdk_luckycycle_ios.
+FOUNDATION_EXPORT double sdk_luckycycle_iosVersionNumber;
+
+//! Project version string for sdk_luckycycle_ios.
+FOUNDATION_EXPORT const unsigned char sdk_luckycycle_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk_luckycycle_ios/PublicHeader.h>
+
+
